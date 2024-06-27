08c588ae3393a85f655229b829358ab1122e3dc3
disjfjdizmfnkf <3037650079@qq.com>
2024-06-27 17:33:51 +0800

    feat: 用户私聊

--- src/index.js
+++ src/index.js
@@ -11,24 +11,46 @@ const io = require('socket.io')(http, {
 });
 const cors = require('cors');
 
-// 使用CORS中间件
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true
 }));
 
+// 用于存储用户连接
+const users = new Map();
+
 io.on('connection', (socket) => {
     console.log('用户连接');
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+    // 用户登录
+    socket.on('login', (userId) => {
+        users.set(userId, socket.id);
+        console.log(`用户 ${userId} 登录`);
+    });
+
+    // 私聊消息
+    socket.on('private message', ({ to, message }) => {
+        const receiverSocket = users.get(to);
+        if (receiverSocket) {
+            io.to(receiverSocket).emit('private message', {
+                from: socket.id,
+                message: message
+            });
+        }
     });
 
     socket.on('disconnect', () => {
-        console.log('用户断开连接');
+        // 从用户列表中移除断开连接的用户
+        for (let [userId, socketId] of users.entries()) {
+            if (socketId === socket.id) {
+                users.delete(userId);
+                console.log(`用户 ${userId} 断开连接`);
+                break;
+            }
+        }
     });
 });
 
 http.listen(3001, () => {
-    console.log(`服务器运行在 http://localhost:3001`);
+    console.log(`socket服务器运行在 http://localhost:3001`);
 });
